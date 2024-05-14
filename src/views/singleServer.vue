fc0c1a1167f8f5808e4a1cb665517b510add5cde
xhz13 <17420212201032@stu.xmu.edu.cn>
2024-05-14 22:05:50 +0800

    modified:   src/utils/MM1.ts 	modified:   src/utils/randnumber.ts 	modified:   src/views/singleServer.vue

--- src/views/singleServer.vue
+++ src/views/singleServer.vue
@@ -1,13 +1,62 @@
 <script setup lang="ts">
+import  { ref } from 'vue';
 import headBar from '../components/headBar.vue';
+import { mm1 } from '../utils/MM1';
+
+
+const num_delays_required = ref(0);
+const mean_interarrival_in= ref(0);
+const mean_service_in   = ref(0);
+const Average_delay = ref(0);
+const Average_number = ref(0);
+const Server_utilization = ref(0);
+const simulation_ended = ref(0);
+
+
+const updateResults = () => {
+  [Average_delay.value, Average_number.value, Server_utilization.value, simulation_ended.value] = mm1(num_delays_required.value, mean_interarrival_in.value, mean_service_in.value);
+};
+
+
+const text = ref('Server Experiment');
+const handleMouseOver = () => {
+    text.value = 'made by 熊志鹏、吴秋尧';
+};
+
+const handleMouseOut = () => {
+    text.value = 'Server Experiment';
+};
+
 </script>
 
 <template>
     <div>
         <headBar/>
+        <div class="container">
+            <p v-on:mouseover="handleMouseOver" v-on:mouseout="handleMouseOut">{{ text }}</p>
+        <div class="input">
+            <el-input v-model="num_delays_required" style="width: 240px" placeholder="num_delays_required" />
+            <el-input v-model="mean_interarrival_in" style="width: 240px" placeholder="Please input" />
+            <el-input v-model="mean_service_in" style="width: 240px" placeholder="Please input" />
+        </div>
+            <el-button type="primary"  @click="updateResults" >Calculate!</el-button>
+        </div>
     </div>
 </template>
 
 <style scoped>
+.container{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+}
+.input{
+    display: flex;
+    flex-direction: column;
+}
+
 
 </style>
+
