079c54c58080fada7d597c108636c7a972251162
xhz13 <17420212201032@stu.xmu.edu.cn>
2024-06-06 16:05:11 +0800

    modified:   src/utils/inv.ts 	modified:   src/views/InventorySystem.vue

--- src/utils/inv.ts
+++ src/utils/inv.ts
@@ -36,7 +36,6 @@ export function inv (num_policies:any){
         /* If the event just executed was not the end-simulation event (type 3),
            continue simulating.  Otherwise, end the simulation for the current
            (s,S) pair and go on to the next pair (if any). */
-
         } while (next_event_type != 3);
     }
 }
@@ -54,83 +53,61 @@ function initialize(initial_inv_level:number,mean_interdemand:number,num_months:
     time_next_event[4] = 0.0;
 }
 
+
+export function initialize2()
 // function expon (mean_interdemand:number):number{
 //     return 1;
 // }
 
 function timing()  /* Timing function. */
 {
-    var   i;
-    var min_time_next_event = 1.0e+29;
-
+    let   i;
+    let min_time_next_event = 1.0e+29;
     next_event_type = 0;
-
     /* Determine the event type of the next event to occur. */
-
     for (i = 1; i <= num_events; ++i)
         if (time_next_event[i] < min_time_next_event) {
             min_time_next_event = time_next_event[i];
             next_event_type     = i;
         }
-
     /* Check to see whether the event list is empty. */
-
     if (next_event_type == 0) {
-
         /* The event list is empty, so stop the simulation */
     }
-
     /* The event list is not empty, so advance the simulation clock. */
-
     sim_time = min_time_next_event;
 }
 
 function order_arrival()  /* Order arrival event function. */
 {
     /* Increment the inventory level by the amount ordered. */
-
     inv_level += amount;
-
     /* Since no order is now outstanding, eliminate the order-arrival event from
        consideration. */
-
     time_next_event[1] = 1.0e+30;
 }
-
 
 function demand()  /* Demand event function. */
 {
     /* Decrement the inventory level by a generated demand size. */
-
-    //这里可能会出问题
     inv_level -= randomInteger(prob_distrib_demand);
-
     /* Schedule the time of the next demand. */
-
     time_next_event[2] = sim_time + expon(mean_interdemand);
 }
-
 
 function evaluate()  /* Inventory-evaluation event function. */
 {
     /* Check whether the inventory level is less than smalls. */
-
     if (inv_level < smalls) {
-
         /* The inventory level is less than smalls, so place an order for the
            appropriate amount. */
-
         amount               = bigs - inv_level;
         total_ordering_cost += setup_cost + incremental_cost * amount;
-
         /* Schedule the arrival of the order. */
-
         time_next_event[1] = sim_time + uniform(minlag, maxlag);
     }
-
     /* Regardless of the place-order decision, schedule the next inventory
        evaluation. */
-
     time_next_event[4] = sim_time + 1.0;
 }
 
@@ -158,33 +135,25 @@ function update_time_avg_stats()  /* Update area accumulators for time-average
 function expon(mean: number): number   /* Exponential variate generation function. */
 {
     /* Return an exponential random variate with mean "mean". */
-
     return -mean *  Math.log(lcgrand(1));
 }
 
-
-function  randomInteger(probDistrib: number[]): number{ /* Random integer generation
-                                             function. */
+function  randomInteger(probDistrib: number[]): number{ /* Random integer generation function. */
     let i = 1;
     let u = lcgrand(1);
     /* Generate a U(0,1) random variate. */
-
     u = lcgrand(1);
-
     /* Return a random integer in accordance with the (cumulative) distribution
        function prob_distrib. */
-
     while (u >= probDistrib[i]) {
         i++;
     }
     return i;
 }
 
-
 function uniform(a:number, b:number)  /* Uniform variate generation function. */
 {
     /* Return a U(a,b) random variate. */
-
     return a + lcgrand(1) * (b - a);
 }
 
--- src/views/InventorySystem.vue
+++ src/views/InventorySystem.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 
+
+
+
 </script>
 
 
